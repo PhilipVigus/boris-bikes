617de907189af3ca1c4ec29bf284acf9f57ad558
Student <student@Makerss-Air-2.fritz.box>
2020-03-18 14:42:39 +0000

    Implement release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
+  # alternate syntax
+  # it { is_expected.to respond_to :release_bike }
+    it 'releases its bike' do
+      expect(subject).to respond_to :release_bike
+    end
 end
